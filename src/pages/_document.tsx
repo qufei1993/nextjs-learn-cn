16e8f7ca598f4bfeeae46e1c90731517b82353ed
五月君 <qzfweb@gmail.com>
2024-01-24 21:11:53 +0800

    fix: baidu statistics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,24 +1,18 @@
 import { Head, Html, Main, NextScript } from "next/document";
-
-import Script from "next/script";
-
+const inlineScript = `
+  var _hmt = _hmt || [];
+  (function() {
+    var hm = document.createElement("script");
+    hm.src = "https://hm.baidu.com/hm.js?d1a3a3a4d626d62cd30aa268728916d5";
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(hm, s);
+  })();
+`;
 export default function Document() {
   return (
     <Html lang="en">
       <Head>
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
-              var _hmt = _hmt || [];
-              (function() {
-                var hm = document.createElement("script");
-                hm.src = "https://hm.baidu.com/hm.js?d1a3a3a4d626d62cd30aa268728916d5";
-                var s = document.getElementsByTagName("script")[0];
-                s.parentNode.insertBefore(hm, s);
-              })();
-            `,
-          }}
-        />
+        <script dangerouslySetInnerHTML={{ __html: inlineScript }} />
       </Head>
       <body>
         <Main />
