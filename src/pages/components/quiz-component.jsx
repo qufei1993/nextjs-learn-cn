9b37e90f2edc62fc974f4522374390a83904db26
五月君 <qzfweb@gmail.com>
2024-05-04 20:41:45 +0800

    fix: generate md5

--- src/pages/components/quiz-component.jsx
+++ src/pages/components/quiz-component.jsx
@@ -16,7 +16,6 @@ const QuizComponent = ({ name, answers = [], correctAnswer }) => {
   const [selectedAnswer, setSelectedAnswer] = useState('');
   const [answerIndex, setAnswerIndex] = useState(-1);
   const [result, setResult] = useState('');
-  const hashCode = generateMD5(name);
 
   const handleAnswerChange = (event, index) => {
     setResult('');
@@ -48,12 +47,12 @@ const QuizComponent = ({ name, answers = [], correctAnswer }) => {
               value={answer}
               checked={selectedAnswer === answer}
               onChange={(e) => handleAnswerChange(e, index)}
-              id={`${hashCode}-${index}`}
+              id={`${generateMD5(name)}-${index}`}
               name={name}
               type="radio"
               class="h-4 w-4 border-gray-300 text-indigo-600 focus:ring-indigo-600"
             />
-            <label htmlFor={`${hashCode}-${index}`} class="block text-sm font-medium leading-6">{AnswerSequence[index]}：{answer}</label>
+            <label htmlFor={`${generateMD5(name)}-${index}`} class="block text-sm font-medium leading-6">{AnswerSequence[index]}：{answer}</label>
           </div>)
         }
       </div>
